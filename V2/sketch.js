08de07b54f4cdcdea893e82677a8a023dafc58a9
KhanhNguyen <ankhanh1566@gmail.com>
2021-11-14 11:30:11 +0700

    U

--- V2/sketch.js
+++ V2/sketch.js
@@ -1,15 +1,63 @@
 let img;
 function preload() {
   img = loadImage("Animation_0.png");
-}
-function setup() {
-  createCanvas(400, 400);
-  background(100);
+  // img = loadImage("Animation_1.png");
+  // img = loadImage("Animation_2.png");
+  // img = loadImage("Animation_3.png");
+  // img = loadImage("Animation_4.png");
+  // img = loadImage("Animation_5.png");
 }
 
+class Player {
+  constructor() {
+    this.pos = createVector(0, 0);
+    this.lookAt = createVector(0, 0);
+    this.normal = createVector(0, -1);
+  }
+  drawPlayer() {
+    // console.log(this.pos, this.lookAt);
+    let vec = p5.Vector.sub(this.lookAt, this.pos)
+    let angle = this.normal.angleBetween(vec);
+    // print(angle);
+
+    push();
+    translate(this.pos);
+    rotate(angle);
+    texture(img);
+    noStroke();
+    rect(0, 0, 196, 196);
+    pop();
+  }
+  update(lookAt) {
+    this.lookAt = lookAt;
+    if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
+      this.pos.x -= 5;
+    } else if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
+      this.pos.x += 5;
+    } else if (keyIsDown(UP_ARROW) || keyIsDown(87)) {
+      this.pos.y -= 5;
+    } else if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
+      this.pos.y += 5;
+    }
+  }
+}
+let player;
+
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  frameRate(60); // Attempt to refresh at starting FPS
+  rectMode(CENTER);
+  angleMode(DEGREES);
+  player = new Player();
+}
 function draw() {
   // background(255, 255, 255, 25);
-  rotate(PI / 3.0);
-  image(img, 100, 100);
 
+  translate(-width / 2, -height / 2);
+  background(100);
+
+  let mouse = createVector(mouseX, mouseY);
+  player.update(mouse);
+  player.drawPlayer();
+  
 }
---
+++ V2/try.js
@@ -0,0 +1,16 @@
+function keyPressed() {
+    console.log("keyPressed", keyCode);
+  }
+  function keyTyped() {
+    console.log("keyTyped", key);
+  }
+  function keyReleased() {
+    console.log("keyReleased", key);
+}
+  
+
+  // a 65
+  // d 68
+  // w 87
+  // s 83
+//   https://keycode.info/
